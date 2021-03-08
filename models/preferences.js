482c1c501fcc27aa02ba138ef0f6c2148acf539d
Nino-dev <heroxisamoris@gmail.com>
2021-03-08 14:29:25 +0100

    Created schema and post form data into array (db), update & delete, added styling

---
+++ models/preferences.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const db = require('../connection')
+
+const PreferenceSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true
+    },
+    brands: {
+      type: Array,
+      required: true
+    }
+});
+
+const preferences = mongoose.model('preferences', PreferenceSchema)
+
+module.exports = preferences
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const connectDB = require('./database')
 const path = require('path');
 const slug = require('slug');
 const bodyParser = require('body-parser');
@@ -8,8 +7,16 @@ const jsonParser = bodyParser.json()
 const indexRouter = require('./routes/index');
 const successRouter = require('./routes/success');
 
+// Models
+const preferences = require('./models/preferences');
+
+//DB Connection
+require('dotenv').config();
+const mongoose = require('mongoose');
+const connectDB = require('./connection');
+connectDB()
+
 const app = express();
-connectDB();
 
 app.set('views', path.join(__dirname, 'views/pages/'));
 app.set('view engine', 'ejs');
@@ -18,10 +25,61 @@ app.use(bodyParser.json());
 app.use("/public", express.static(path.join(__dirname, '/public')));
 
 app.get('/', indexRouter);
-app.get('/success', successRouter);
-app.post('/success', (req, res) => {
-  const data = req.body;
-  res.render('success', { data });
+
+//POST form data to DB
+app.post('/add', async (req, res) => {
+  try {
+    const username = req.body.username
+    const brands = req.body.brands
+    const preference = new preferences({
+      username: username,
+      brands: brands
+    });
+    await preference.save()
+    .then(() => {res.redirect('success')
+  })
+    } catch(err) {
+        console.log(err);
+        res.status(500).send();
+    }
+  })
+
+// Update Array
+app.post('/update', async (req, res) => {
+  try {
+    const brands = req.body.brands
+    let preference = await preferences.findOne({
+          username: "nino"
+        });
+        await preferences.updateOne({}, {
+          brands: brands
+        });
+    await preference.save()
+    .then(() => {res.redirect('success')
+  })
+    } catch(err) {
+        console.log(err);
+        res.status(500).send();
+    }
+  })
+
+//Delete preferences
+app.post('/delete', async (req, res) => {
+    try {
+      const username = req.body.username
+      await preferences.findOneAndDelete({
+        username: username
+      }).exec();
+      res.redirect('/');
+    } catch (error) {
+      res.status(500).send();
+    }
+  });
+
+//Read data from DB
+app.get('/success', async (req, res) => {
+  const dataPreferences = await preferences.findOne();
+  res.render('success', {data: dataPreferences});
 });
 
 app.listen(3000);
